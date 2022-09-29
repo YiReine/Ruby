634a606489942034b29199216d78d646bd4b1189
YiReine <ynhi2102001@gmail.com>
2022-09-29 17:08:35 +0700

    Exercise 2: Import file data by ActiveRecord

---
+++ inputF.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby
+require "rails"
+require "active_record"
+require "csv"
+require "pg"
+require "benchmark"
+def caculate_time
+time = Benchmark.realtime do
+        yield
+end
+        puts "Time: #{time.round(2)}"
+end
+
+
+
+
+ActiveRecord::Base.establish_connection(:adapter => "postgresql",
+					:host => "localhost",
+                                        :username => "postgres",
+                                        :password => "123",
+                                        :database => "nhi")
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :external_post_id, presence: true
+  validates :external_post_id, uniqueness: true
+end
+Post.connection.execute <<-SQL
+  DROP TABLE IF EXISTS post_imports;
+  CREATE  TABLE post_imports
+  (
+    name character varying,
+    email character varying,
+    phone character varying,
+    address character varying,
+    date_of_birth timestamp with time zone,
+    profile character varying,
+
+    primary key(name)
+  )
+SQL
+
+
+caculate_time do
+File.open('ha.csv', 'r') do |file|
+  Post.connection.raw_connection.copy_data %{copy post_imports from stdin with csv delimiter ',' quote '"'} do
+    file.gets
+    while line = file.gets do
+      Post.connection.raw_connection.put_copy_data line
+    end
+  end
+end
+
+Post.connection.execute <<-SQL
+  insert into post_imports(name, email, phone, address, date_of_birth, profile)
+  select name, email, phone, address, date_of_birth, profile
+  from post_imports
+  on conflict(name) do 
+  update set
+	name = EXCLUDED.name,
+	email = EXCLUDED.email,
+	phone = EXCLUDED.phone,
+	address = EXCLUDED.address,
+	date_of_birth = EXCLUDED.date_of_birth,
+	profile = EXCLUDED.profile
+  returning name
+SQL
+
+end
+
+#Time run: 17s
